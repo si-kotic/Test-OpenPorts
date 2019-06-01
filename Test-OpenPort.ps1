3e286647784bb51bab6466a50d1d6f707d038210
Simon Brown <simon.brown@bridgepartners.co.uk>
2019-06-01 18:21:53 +0100

    Changed parameter name from ComputerName to Address

--- Test-OpenPort.ps1
+++ Test-OpenPort.ps1
@@ -3,17 +3,17 @@ Function Test-OpenPort {
     Param (
         [Parameter(Mandatory=$true,
         ValueFromPipeline=$true)]
-        [string]$ComputerName,
+        [string]$Address,
         [Parameter(Mandatory=$true,
         ValueFromPipeline=$true)]
         [int32]$Port
     )
     Try {
         $tcpConnection = New-Object System.Net.Sockets.TcpClient
-        $tcpConnection.Connect($ComputerName, $Port)
-        Write-Host "Successfully connected to $ComputerName on Port $Port" -ForegroundColor Green
+        $tcpConnection.Connect($Address, $Port)
+        Write-Host "Successfully connected to $Address on Port $Port" -ForegroundColor Green
         $tcpConnection.Close()
     } Catch {
-        Write-Host "Could not connect to $Computer on Port $Port" -ForegroundColor Red
+        Write-Host "Could not connect to $Address on Port $Port" -ForegroundColor Red
     }
 }
